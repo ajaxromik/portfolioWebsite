f60b5127ee906c88892b9dd8ea389833ec3b4f1b
aromik <awillcarr@gmail.com>
2026-02-23 17:49:27 -0500

    filled out skills more

--- src/views/CredentialsView.vue
+++ src/views/CredentialsView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="mt-5 bg-new-light text-new-dark">
-    <section class="py-5 px-4 container">
+  <div class="mt-5 bg-new-light text-new-dark flex-grow-1 d-flex flex-column">
+    <section class="flex-grow-1 d-flex flex-column justify-content-center py-5 px-4 container">
       <div class="row mx-md-0 gx-5 text-center">
         <h2 class="fw-bold col-12 border-bottom pb-1">Education</h2>
         <div class="col-6 col-md-12">
@@ -13,25 +13,26 @@
           <p class="my-2"><span class="fw-semibold">Honors:</span> GPA 3.7, President's List, Dean's List</p>
           <p class="mb-0"><span class="fw-semibold">Highlight Courses:</span> Software Engineering, Data Structures & Algorithms, 
             <br>
-            Visual Analytics, and Database Systems and Design</p>
+            Visual Analytics, and Database Systems and Design
+          </p>
         </div>
       </div>
     </section>
 
-    <section class="py-5 bg-light-blue">
+    <section class="flex-grow-1 d-flex flex-column justify-content-center py-5 bg-light-blue">
       <div class="container text-center">
         <h2 class="mb-4 fw-bold pb-1">Technical Skills</h2>
         <div class="row justify-content-center">
           <div class="col-md-10 text-start fs-5">
-            <p><strong>Programming Languages & Databases:</strong> Java, PHP, JavaScript, Python, C++, MySQL, MongoDB, Neo4j</p>
-            <p><strong>Other Relevant Skills:</strong> Web Development, Relational Database Design, Object-Oriented Design</p>
-            <p><strong>Soft Skills:</strong> Adaptability, effective communication, negotiation, positive attitude, collaboration over competition</p>
+            <p><strong>Languages and Other Technologies:</strong> PHP, MySQL, Linux CLI, Python, Java, Bootstrap, Sass, Vue.js, React, React Native, Smarty, JavaScript, D3.js, HTML, CSS, C++, MongoDB, Neo4j</p>
+            <p><strong>Other Relevant Skills:</strong> Prompt Engineering, Unit Testing, Code Review, Web Development, Relational Database Design, Object-Oriented Design, Visual Analytics, Version Control, Data Cleaning</p>
+            <p><strong>Soft Skills:</strong> Adaptability, Effective Communication, Attention to Detail, Problem Solving, Prioritization, Techical Writing</p>
           </div>
         </div>
       </div>
     </section>
 
-    <section class="py-5 text-center">
+    <section class="flex-grow-1 d-flex flex-column justify-content-center py-5 text-center">
       <div class="container">
         <h2 class="mb-4 fw-bold">Resume</h2>
         <p class="fs-5 mb-4">You can find the formal document below.</p>
