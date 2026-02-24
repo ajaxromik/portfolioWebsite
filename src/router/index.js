334d9e366821ff4be796ccaf41b8375d0bbe576c
aromik <awillcarr@gmail.com>
2026-02-23 19:25:44 -0500

    Added a 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PageNotFound from '../views/PageNotFoundView.vue'
 import HomeView from '../views/HomeView.vue'
 import ExperienceView from '../views/ExperienceView.vue'
 import CredentialsView from '../views/CredentialsView.vue'
@@ -6,9 +7,26 @@ import CredentialsView from '../views/CredentialsView.vue'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/experience', name: 'experience', component: ExperienceView },
-    { path: '/credentials', name: 'credentials', component: CredentialsView }
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/experience',
+      name: 'experience',
+      component: ExperienceView
+    },
+    {
+      path: '/credentials',
+      name: 'credentials',
+      component: CredentialsView
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'Page Not Found',
+      component: PageNotFound
+    }
   ],
   linkActiveClass: 'active'
 })
---
+++ src/views/PageNotFoundView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="py-4"></div>
+    <div class="py-5 my-auto d-flex flex-column justify-content-center text-center">
+      <h1 class="display-3 fw-bold text-new-dark">404 - Page Not Found</h1>
+      <p class="fs-5">Sorry, the page you're looking for doesn't exist.</p>
+      <div class="d-flex justify-content-center gap-3">
+        <router-link to="/" class="btn btn-secondary btn-lg">Take me home</router-link>
+      </div>
+    </div>
+  </div>
+</template>
