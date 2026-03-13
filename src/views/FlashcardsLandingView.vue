f1709f21252d086f97755a7fd1cb995b1a7c6ca9
aromik <awillcarr@gmail.com>
2026-03-13 11:11:17 -0400

    landing page ux updates for responsiveness

--- src/views/FlashcardsLandingView.vue
+++ src/views/FlashcardsLandingView.vue
@@ -13,13 +13,13 @@ const isSignedIn = computed(() => !!user.value && !user.value.isAnonymous);
     <section class="landing-section d-flex align-items-center bg-light text-center pt-5">
       <div class="container">
         <div class="row justify-content-center">
-          <div class="col-12 col-lg-9">
+          <div class="col-9">
             <h1 class="display-4 fw-bold">Flashcards Anywhere</h1>
             <h4 class="text-secondary fw-normal">
               A free, flashcard application (unlike Quizlet) that links to any Google account.
-              <br />
+              <br class="d-none d-xl-block" />
               I built it with a Spaced Repetition System to order the cards effectively.
-              <br />
+              <br class="d-none d-xl-block" />
               More features to come soon.
             </h4>
             <h5 class="fw-normal text-secondary mb-4">
@@ -46,7 +46,7 @@ const isSignedIn = computed(() => !!user.value && !user.value.isAnonymous);
     <section class="py-5 bg-new-light">
       <div class="container text-center d-flex flex-column justify-content-center align-items-center">
         <h2 class="fw-bold mb-4">Try a quick demo</h2>
-        <div class="bg-light rounded-3 px-4 mb-4 pt-2 pb-3">
+        <div class="bg-light rounded-3 px-4 mb-4 pt-2 pb-3 w-75">
           <p class="text-secondary fs-5 fw-semibold mt-0 mb-1">The keyboard can control the cards too.</p>
           <p class="text-secondary text-start mb-0">
             <kbd>Spacebar</kbd> flips the card.
@@ -72,9 +72,9 @@ const isSignedIn = computed(() => !!user.value && !user.value.isAnonymous);
     </section>
 
     <section class="py-5 bg-light">
-      <div class="container text-center d-flex flex-column justify-content-center align-items-center">
+      <div class="container text-center d-flex flex-column justify-content-center align-items-center w-75">
         <h2 class="fw-bold mb-3">Based on Learning Optimization Science</h2>
-        <p class="fs-5 text-secondary mb-4 w-75">
+        <p class="fs-5 text-secondary mb-4">
           If you're curious about the math behind the algorithm, you can check out the <a href="https://www.supermemo.com/en/blog/application-of-a-computer-to-improve-the-results-obtained-in-working-with-the-supermemo-method" target="_blank" rel="noopener">SuperMemo-2 Algorithm Documentation</a>.
           I came across this method while improving my studying skills, and I found the topic quite interesting.
           Another source that can show practical applications of learning science would be <i>Fluent Forever</i> by Gabriel Wyner.
